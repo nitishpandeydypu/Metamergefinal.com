ae808630243b63bc252ef7a78309b545891d59bb
nitishpandeydypu <145127561+nitishpandeydypu@users.noreply.github.com>
2026-04-12 18:28:50 +0530

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -342,7 +342,7 @@ export default function Shop() {
                                     href={`/shop/${p.id}`}
                                     className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-semibold hover:bg-blue-500"
                                 >
-                                    {p.cta}
+                            
                                 </Link>
                             </div>
                         </div>
