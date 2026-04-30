58ae2e8948120fd388b5d459cde37d7c06ee74d3
nitishpandeydypu <145127561+nitishpandeydypu@users.noreply.github.com>
2026-04-30 22:23:08 +0530

    Update ClientLayout.tsx

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -14,7 +14,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
       <main>{children}</main>
 
-      <Footer />
+  
 
       {/* 🔥 SUPPORT MODAL (YAHI FINAL HAI) */}
       {openSupport && (
